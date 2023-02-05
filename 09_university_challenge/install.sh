d9bb417332c0db77b2cfd4f8fa9b7a75d654adf0
jonay2000 <jonabent@gmail.com>
2023-02-05 14:19:41 +0100

    merge done!

--- 09_university_challenge/install.sh
+++ 09_university_challenge/install.sh
@@ -5,6 +5,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 YEAR="year9"
 YEARPATH="archlive/airootfs/home/$YEAR"
 
-cp -r $SCRIPT_DIR/project $YEARPATH/rusttracer
-mkdir $YEARPATH/.cargo
-cp $SCRIPT_DIR/config.toml $YEARPATH/.cargo/config.toml
+cp -r $SCRIPT_DIR/READMEATOXFORD.md $YEARPATH
